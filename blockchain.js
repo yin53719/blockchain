5e0d53bfdf57161ca909edd1dfebe1487c803e48
hardy <yincc1@yonyou.com>
2019-03-13 22:44:40 +0800

    'updata'

--- blockchain.js
+++ blockchain.js
@@ -1,32 +1,51 @@
 const crypto = require('crypto')
-
+const initBlock = {
+  index: 0,
+  previousHash: '0',
+  timestamp: 1538669227813,
+  data: 'Welcome to iblockchain!',
+  hash: '00sssaa1fbf27775ab79612bcb8171b3a9e02efe32fa628450ba6e729cf03996',
+  nonce: 979911
+}
 class Blockchain {
   constructor(){
-      this.blockchain = []
-      this.data = []
-      this.difficulty = 4;
+      this.blockchain = [initBlock]
+      this.data = 'hardy'
+      this.difficulty = 5;
       // const hash = this.computeHash(0,'0',new Date().getTime(),'hello-woniuchain',1)
       // console.log(hash)
   }
    // 挖矿
   mine(){
-    let nonce = 0;
-    const index = 0;
-    const data = 'hello woniu-chain'
-    const prevHash = '0'
-    let tiemstamp = 111111111
-    let hash = this.computeHash(nonce,prevHash,tiemstamp,data,nonce)
+    const newBlock = this.generatnewBlock()
 
-    while(hash.slice(0,4)!=='0000'){
-      nonce += 1
-      hash = this.computeHash(nonce,prevHash,tiemstamp,data,nonce)
-      console.log(nonce,hash)
+    if(this.isValidBlock(newBlock)){
+      this.blockchain.push(newBlock)
+    }else{
+      console.log('Error,Invalid Block');
     }
   }
-
+  getLastBlock(){
+    return this.blockchain[this.blockchain.length-1]
+  }
   // 生成新区块
   generatnewBlock(){
+    let nonce = 0;
+    const index = this.blockchain.length;
+    const data = this.data
+    const prevHash = this.getLastBlock().hash
+    let tiemstamp = new Date().getTime();
+    let hash = this.computeHash(nonce,prevHash,tiemstamp,data,nonce)
 
+    while(hash.slice(0,this.difficulty)!=='0'.repeat(this.difficulty)){
+      nonce += 1
+      tiemstamp = new Date().getTime();
+      hash = this.computeHash(nonce,prevHash,tiemstamp,data,nonce)
+    }
+    
+    return {
+      index,nonce,prevHash,tiemstamp,data,nonce,hash
+    }
   }
   // 计算哈希
   computeHash(index,prevHash,tiemstamp,data,nonce){
@@ -35,8 +54,24 @@ class Blockchain {
                   .update(index + prevHash + tiemstamp + data + nonce)
                   .digest('hex')
   }
-  isValidBlock(){
+  // 校验区块
+  isValidBlock(newBlock){
+    const lastBlock = this.getLastBlock(newBlock)
+    if(newBlock.index !== lastBlock.index+1){
+      console.log(1)
+      return false;
+    }else if(newBlock.tiemstamp <= lastBlock.tiemstamp){
+      console.log(2)
+      return false;
+    }else if(newBlock.prevHash !== lastBlock.hash){
+      console.log(4)
+      return false;
+    }else if(newBlock.hash.slice(0,this.difficulty) !== '0'.repeat(this.difficulty)){
+      console.log(5)
+      return false;
+    }
 
+    return true
   }
   isValidChain(){
 
@@ -47,3 +82,6 @@ class Blockchain {
 let bc = new Blockchain();
 
 bc.mine()
+bc.mine()
+bc.mine()
+console.log(bc.blockchain)
